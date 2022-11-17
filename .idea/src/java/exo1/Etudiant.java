0ac44434e1dfe516039bb217e999a4f49f4bad8b
GueganNicolas <nicolas.guegan@alumni.univ-avignon.fr>
2022-11-17 15:39:05 +0100

    push test

--- .idea/src/java/exo1/Etudiant.java
+++ .idea/src/java/exo1/Etudiant.java
@@ -3,4 +3,10 @@ package exo1;
 public class Etudiant {
     private String nom;
     private String prenom;
+
+    public Etudiant(String nom,String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
 }
